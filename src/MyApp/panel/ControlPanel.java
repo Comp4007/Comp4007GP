6d5d2b866fa49d1b1d571acdead2c089fa39c3d0
chlorella <chlorella2012@gmail.com>
2016-11-23 19:55:17 +0800

    Revert "Revert "Revert "Revert "update of building and controlpanel""""
    This reverts commit aa3b787cd91285c0dc395790fe59a9a7405dcc2b.

--- src/MyApp/panel/ControlPanel.java
+++ src/MyApp/panel/ControlPanel.java
@@ -2,7 +2,10 @@ package MyApp.panel;
 
 import MyApp.Building;
 
+import javax.swing.JFrame;
+
 public class ControlPanel implements Panel{
+	private JFrame frame;
 	
 	private Building building;
 	
@@ -13,17 +16,17 @@ public class ControlPanel implements Panel{
 	@Override
 	public void showInfo() {
 		// TODO Auto-generated method stub
-		System.out.println("Elevator status :");
-	//	for(int i=0;i< ~elevatorcount~ ; i++)
-	//		System.out.println("Elevator "+i + ": " + building.getElevatorStatus());//sample
+		System.out.println("Elevator status: ");
+		System.out.println(building.getElevatorStatus());//sample
 		System.out.println();
-		System.out.println("Elevator queue :");
-	//	for(int i=0;i< ~elevatorcount~ ; i++)
-	//		System.out.println("Queue of Elevator "+i + ": " + building.getThread("e"+i).getQueue());//sample
+		
+		System.out.println("Elevator queue: ");
+		
+		System.out.printf(building.getElevatorQueue());//sample
 		System.out.println();
-		System.out.println("Kiosk queue :");
-	//	for(int i=0;i< ~kioskcount~ ; i++)
-	//		System.out.println("Queue of Elevator "+i + ": " + ~);
+		
+		System.out.println("Kiosk queue: ");
+		System.out.printf(building.getKioskQueue());
 		
 	}
 	
