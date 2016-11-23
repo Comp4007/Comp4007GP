c4b2647a92af35a4cc4585367c5d153b41a6d66d
hkbu-15200183 <15200183@life.hkbu.edu.hk>
2016-11-23 17:30:21 +0800

    sample
    need better understanding of the scripts before implement correct one

--- src/MyApp/panel/ControlPanel.java
+++ src/MyApp/panel/ControlPanel.java
@@ -13,8 +13,18 @@ public class ControlPanel implements Panel{
 	@Override
 	public void showInfo() {
 		// TODO Auto-generated method stub
-		building.getThread("").getQueue();//sample
-		building.getElevatorStatus();//sample
+		System.out.println("Elevator status :");
+	//	for(int i=0;i< ~elevatorcount~ ; i++)
+	//		System.out.println("Elevator "+i + ": " + building.getElevatorStatus());//sample
+		System.out.println();
+		System.out.println("Elevator queue :");
+	//	for(int i=0;i< ~elevatorcount~ ; i++)
+	//		System.out.println("Queue of Elevator "+i + ": " + building.getThread("e"+i).getQueue());//sample
+		System.out.println();
+		System.out.println("Kiosk queue :");
+	//	for(int i=0;i< ~kioskcount~ ; i++)
+	//		System.out.println("Queue of Elevator "+i + ": " + ~);
+		
 	}
 	
 }
