3a8ebfe522bcdf0c248b3523eacc3d380d37cd2b
hkgsherlock <hkgsherlock@gmail.com>
2016-11-26 03:54:10 +0800

    use meter to sort instead

--- src/MyApp/Floor.java
+++ src/MyApp/Floor.java
@@ -1,24 +1,21 @@
 package MyApp;
 
 import java.io.Serializable;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public final class Floor implements Serializable, Comparable<Floor> {
-    private final String floor;
+    private final String name;
     private final double yDisplacement;
 
     private Floor lowerFloor = null;
     private Floor upperFloor = null;
-    private int sortIndex = 0; // TODO: auto-update
 
-    public Floor(String floor, double yDisplacement) {
-        this.floor = floor;
+    public Floor(String name, double yDisplacement) {
+        this.name = name;
         this.yDisplacement = yDisplacement;
     }
 
-    public String getFloor() {
-        return floor;
+    public String getName() {
+        return name;
     }
 
     public double getYDisplacement() {
@@ -37,34 +34,24 @@ public final class Floor implements Serializable, Comparable<Floor> {
         this.lowerFloor = lowerFloor;
         if (lowerFloor == null) return;
         this.lowerFloor.upperFloor = this;
-        this.updateLowerFloorSortIndex();
     }
 
     public void setUpperFloor(Floor upperFloor) {
         this.upperFloor = upperFloor;
         if (upperFloor == null) return;
         this.upperFloor.lowerFloor = this;
-        this.updateUpperFloorSortIndex();
-    }
-
-    private void updateLowerFloorSortIndex() {
-        Floor prev = this;
-        Floor current = this;
-        for (; (current = current.getLowerFloor()) != null; prev = current) {
-            current.sortIndex = prev.sortIndex - 1;
-        }
-    }
-
-    private void updateUpperFloorSortIndex() {
-        Floor prev = this;
-        Floor current = this;
-        for (; (current = current.getLowerFloor()) != null; prev = current) {
-            current.sortIndex = prev.sortIndex + 1;
-        }
     }
 
     @Override
     public int compareTo(Floor comparing) {
-        return this.sortIndex - comparing.sortIndex;
+        if (this.equals(comparing)) return 0;
+        return (int)this.yDisplacement - (int)comparing.yDisplacement;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null &&
+                (obj.getClass() == Floor.class) &&
+                this.name.equals(((Floor)obj).name);
     }
 }
