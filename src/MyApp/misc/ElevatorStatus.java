212ac99e4eb67d19d22feb79f11a104bf505a208
hkgsherlock <hkgsherlock@gmail.com>
2016-11-26 03:55:17 +0800

    what height? lift carriage? lift shaft?

--- src/MyApp/misc/ElevatorStatus.java
+++ src/MyApp/misc/ElevatorStatus.java
@@ -1,20 +1,27 @@
 package MyApp.misc;
 
+import MyApp.elevator.Elevator;
+
 public class ElevatorStatus {
-	
-	public double height;
-	public double velocity;
+	private Elevator elevator;
+	private double height;
+	private double velocity;
 	private double breakDistance;
 	private double acceleration;
 	
-	public ElevatorStatus(double height, double velocity, double breakDistance, double acceleration) {
+	public ElevatorStatus(Elevator elevator, double height, double velocity, double breakDistance, double acceleration) {
+		this.elevator = elevator;
 		this.height = height;
 		this.velocity = velocity;
 		this.breakDistance = breakDistance;
 		this.acceleration = acceleration;
 	}
-	
-	public double getHeight(){
+
+	public Elevator getElevator() {
+		return elevator;
+	}
+
+	public double getYPosition(){
 		return this.height;
 	}
 	
