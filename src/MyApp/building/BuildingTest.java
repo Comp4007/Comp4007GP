dedb889ad5c2355f40763f0fa556573c35e3ee48
hkgsherlock <hkgsherlock@gmail.com>
2016-11-28 03:02:07 +0800

    add classpath and generate test methods used by JUnit4

---
+++ src/MyApp/building/BuildingTest.java
@@ -0,0 +1,81 @@
+package MyApp.building;
+
+import static org.junit.Assert.*;
+
+public class BuildingTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void startApp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getLogger() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void putThread() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getThread() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getThreads() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getProperty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getElevatorQueue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getKioskQueue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getElevatorStatus() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getTotalDisplacementMeters() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getFloorPositions() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getFloorNames() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getFloorPosition() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void putNewHopRequest() throws Exception {
+
+    }
+
+}
