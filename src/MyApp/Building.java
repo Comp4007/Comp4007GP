e9b8b01f4d1e90537e1729505ddd478cf8ad0433
hkgsherlock <hkgsherlock@gmail.com>
2016-11-27 19:16:37 +0800

    sort out the JavaDocs and comments

--- src/MyApp/Building.java
+++ src/MyApp/Building.java
@@ -34,7 +34,7 @@ public class Building {
     /**
      * This is config file path
      */
-    private final String cfgFName = "etc/MyApp.cfg";
+    private static final String cfgFName = "etc/MyApp.cfg";
     /**
      * Logging module for verbose, debugging and warning/error messages.
      */
@@ -58,10 +58,8 @@ public class Building {
     private final AtomicReference<LinkedHashMap<String, Floor>> arefFloorPositions;
     // http://cookieandcoketw.blogspot.hk/2013/03/java-hashmap-hashtable.html
     // http://www.infoq.com/cn/articles/ConcurrentHashMap
-    /**
-     * // TODO: JavaDoc for the kioskHoppingRequests
-     */
-    private final ConcurrentHashMap<String, Floor> kioskHoppingRequests;
+    //JavaDoc for the kioskHoppingRequests
+//    private final ConcurrentHashMap<String, Floor> kioskHoppingRequests;
     /**
      * Stores all the statuses of all the Elevators inside this Building, as a cache.
      */
@@ -124,8 +122,7 @@ public class Building {
             if (cfgProps.containsKey("FloorPositions")) {
                 Stream<String> s = Arrays.stream(cfgProps.getProperty("FloorPositions").split("\\|"));
                 floorPositions = s.mapToDouble(Double::parseDouble).toArray();
-            }
-            else
+            } else
                 throw new InvalidPropertiesFormatException("missing FloorPositions");
 
             // for key-value pair, asserting array length size is same is required
@@ -142,7 +139,7 @@ public class Building {
                 lowerFloor = floor;
             }
 
-            LinkedHashMap<String, Floor> floorPositions2 = floorPositions1.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e1, LinkedHashMap::new));
+            LinkedHashMap<String, Floor> floorPositions2 = floorPositions1.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
             this.arefFloorPositions = new AtomicReference<>(floorPositions2);
         }
@@ -154,15 +151,16 @@ public class Building {
         log.setUseParentHandlers(false);
         log.addHandler(conHd);
         log.setLevel(Level.INFO);
-        appThreads = new Hashtable<String, AppThread>();
+        appThreads = new Hashtable<>();
 
-        kioskHoppingRequests = new ConcurrentHashMap<>();
+//        kioskHoppingRequests = new ConcurrentHashMap<>();
         elevatorsStatuses = new ConcurrentHashMap<>();
     }
 
     /**
      * Start running up the world of the simulation.
      */
+    @SuppressWarnings("WeakerAccess")
     public void startApp() {
         // This is for elevator use implement by steven and kers
         Timer timer = new Timer("timer", this);
@@ -210,6 +208,7 @@ public class Building {
 
     /**
      * Retrieves the Logger module for recording.
+     *
      * @return The Logger moudle.
      */
     public Logger getLogger() {
@@ -229,6 +228,7 @@ public class Building {
      * E.g.:
      * <code>Eleavtor ((Elevator)(this.getThread("e" + 1))).getStatus()</code>
      * → get the e1(Elevator 2) status
+     *
      * @param id The element identifier for the simulation object.
      */
     public AppThread getThread(String id) {
@@ -237,16 +237,19 @@ public class Building {
 
     /**
      * Get threads from the threads storage that matches the type extending.
+     *
      * @param type The class object of the type that extends the AppThread.
-     * @param <T> The type that extends the AppThread.
+     * @param <T>  The type that extends the AppThread.
      * @return A list of the threads that matches the type extending.
      */
+    @SuppressWarnings({"unchecked", "WeakerAccess"})
     public <T extends AppThread> List<T> getThreads(Class<T> type) {
-        return appThreads.values().stream().filter((t) -> t.getClass() == type).map((t) -> (T)t).collect(Collectors.toList());
+        return appThreads.values().stream().filter((t) -> t.getClass() == type).map(t -> (T) t).collect(Collectors.toList());
     }
 
     /**
      * Get config file key value pair
+     *
      * @param property Key of the configuration property.
      * @return The value of the specified configuration property.
      */
@@ -256,45 +259,47 @@ public class Building {
 
     /**
      * Returns the status of elevators queues in startElevatorStatusCacheThread textual format for user to see.
+     *
      * @return A string representation of the status of the elevators queues.
      * @see ControlPanel
      */
-    public String getElevatorQueue(){
+    public String getElevatorQueue() {
         String geq = "";
-    
-        for(int i = 0; i < Elevator.elevatorCount; i++){
-            HashMap<Integer, String> rq =  this.getThread("e" + i).getQueue();
-            
-             geq+= "Elevator "+i+ ": " + rq +"\n";
+
+        for (Elevator e : getThreads(Elevator.class)) {
+            geq += "Elevator " + e.getID() + ": " + e.getQueue() + "\n";
         }
         return geq;
     }
 
     /**
      * Returns the status of kiosk queue in startElevatorStatusCacheThread textual format for user to see.
+     *
      * @return A string representation of the status of the kiosk queue.
      * @see ControlPanel
      */
-    public String getKioskQueue(){
+    public String getKioskQueue() {
         String gkq = "";
-        
-        for(int i = 0; i < Kiosk.kioskCount; i++){
-            HashMap<Integer, String> rq =  this.getThread("k" + i).getQueue();
+
+        for (int i = 0; i < Kiosk.kioskCount; i++) {
+            HashMap<Integer, String> rq = this.getThread("k" + i).getQueue();
             // demo of how to get queue of kiosk, 
             // can also use this to get queue of elevator
-             gkq+= "Floor "+i+ ": " + rq +"\n";
+            gkq += "Floor " + i + ": " + rq + "\n";
         }
-        
+
         // ElevatorStatus status = ((Elevator)(this.getThread("e" + 1))).getStatus();
         // Algorithm stuff
-        
+
         return gkq;
     }
 
     // TODO: remove if wasting space
+
     /**
      * [DEPRECATED]
      * Get all statuses of different elevators accordingly.
+     *
      * @return An array list of elevator statuses.
      */
     @Deprecated
@@ -312,6 +317,7 @@ public class Building {
 
     /**
      * Get the total displacement that an elevator may travel for vertically within the building.
+     *
      * @return The displacement, in meters.
      */
     public final int getTotalDisplacementMeters() {
@@ -320,6 +326,7 @@ public class Building {
 
     /**
      * Get an dictionary for all floors, with their names and the displacement that matches the vertical position of the floor.
+     *
      * @return A <code>Map</code> object that contains startElevatorStatusCacheThread list of: <br/>
      * <code>String</code> of the floor names and <br/>
      * <code>Floor</code> of the real floor object, which contains displacement that matches the vertical position of the floor in meters.
@@ -330,6 +337,7 @@ public class Building {
 
     /**
      * Get all the floor names as a String array.
+     *
      * @return A string array of all floor names.
      */
     public final String[] getFloorNames() {
@@ -339,6 +347,7 @@ public class Building {
 
     /**
      * Get floor object by the floor name.
+     *
      * @param floorName The alias of the floor that is human-readable.
      * @return The floor object that is inside the building, containing the information and specifications about the floor.
      */
@@ -348,13 +357,11 @@ public class Building {
 
     /**
      * Provides a method for Kiosk to put a new hop request for an elevator to stop at.
-     * @param kiosk The source Kiosk that puts the request into this Building.
+     *
+     * @param kiosk     The source Kiosk that puts the request into this Building.
      * @param destFloor The destination floor that, after passenger boarding from the source floor, which floor to let passenger alight.
-     * @return The elevator that is assigned for passenger to board.
-     * @throws IndexOutOfBoundsException Throws when <ul>
-     *     <li>destFloor key not exist in floorPositions;</li>
-     *     <li>destFloor key not exist in floorPositions;</li>
-     * </ul>
+     * @return The elevator that is assigned for passenger to board, or <code>null</code> if retried <code>putStoppingHopMaxRetries</code> times.
+     * @throws IndexOutOfBoundsException Throws when floor name, which is value of <code>destFloor</code>, does not exist in <code>floorPositions</code>.
      */
     public synchronized Elevator putNewHopRequest(Kiosk kiosk, String destFloor) throws IndexOutOfBoundsException {
         Floor src = kiosk.getFloor();
@@ -377,14 +384,14 @@ public class Building {
         for (int i = 0; i < ess.size() && tries < putStoppingHopMaxRetries; i = ++tries % ess.size()) {
             ElevatorStatus es = ess.get(i);
 
-//            // TODO: START - test if correct for all cases for following block of formulas
-//            // check if can stop within floor
-//            int direction = goingUp ? 1 : -1;
-//            double displacementElevatorStop = es.getYPosition() + es.getDirection() * es.getBreakDistance();
-//            double displacementFloor = dest.getYDisplacement();
-//            if (direction * displacementElevatorStop < direction * displacementFloor) // eg: 35 < 30 (going up) = false -> fail; -40 < -20 (going down) = true -> work
-//                continue;
-//            // TODO: END - test if correct for all cases for following block of formulas
+            // ignoring such code, calculate if may stop within floor level by the Elevator itself.
+            /*
+            int direction = goingUp ? 1 : -1;
+            double displacementElevatorStop = es.getYPosition() + es.getDirection() * es.getBreakDistance();
+            double displacementFloor = dest.getYDisplacement();
+            if (direction * displacementElevatorStop < direction * displacementFloor) // eg: 35 < 30 (going up) = false -> fail; -40 < -20 (going down) = true -> work
+                continue;
+            */
 
             // push back to the lift to update its next destination.
             if (es.getElevator().putNewDestination(dest)) {
@@ -393,7 +400,7 @@ public class Building {
             }
         }
 
-        // return null if retries but failed at all
+        // return null if retried many times but failed at all
         return null;
     }
 }
